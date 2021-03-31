74cc57d73635a6a3a46a5278635cac2bf104de2f
Klaus Vitor Marques Bighi <56765311+KlausBighi@users.noreply.github.com>
2021-03-31 18:41:11 -0300

    Projeto pronto v1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Calcular_Consumo_do_Carro
+{
+    partial class CalcularConsumo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calcularButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.rendimentoLabel = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.consumoLabel = new System.Windows.Forms.Label();
+            this.kmTextBox = new System.Windows.Forms.TextBox();
+            this.litrosTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // calcularButton
+            // 
+            this.calcularButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.calcularButton.Location = new System.Drawing.Point(69, 156);
+            this.calcularButton.Name = "calcularButton";
+            this.calcularButton.Size = new System.Drawing.Size(252, 60);
+            this.calcularButton.TabIndex = 0;
+            this.calcularButton.Text = "Calcular";
+            this.calcularButton.UseVisualStyleBackColor = true;
+            this.calcularButton.Click += new System.EventHandler(this.calcularButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(26, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "&Km:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(10, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Litros:";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Segoe UI", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label3.Location = new System.Drawing.Point(200, 15);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(135, 13);
+            this.Label3.TabIndex = 1;
+            this.Label3.Text = "Rendimento – Km / Litros";
+            // 
+            // rendimentoLabel
+            // 
+            this.rendimentoLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.rendimentoLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rendimentoLabel.Location = new System.Drawing.Point(200, 28);
+            this.rendimentoLabel.Name = "rendimentoLabel";
+            this.rendimentoLabel.Size = new System.Drawing.Size(121, 27);
+            this.rendimentoLabel.TabIndex = 1;
+            this.rendimentoLabel.Tag = "1";
+            this.rendimentoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Font = new System.Drawing.Font("Segoe UI", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label5.Location = new System.Drawing.Point(200, 89);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(121, 13);
+            this.Label5.TabIndex = 1;
+            this.Label5.Text = "Consumo – Litros / Km";
+            // 
+            // consumoLabel
+            // 
+            this.consumoLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.consumoLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.consumoLabel.Location = new System.Drawing.Point(200, 102);
+            this.consumoLabel.Name = "consumoLabel";
+            this.consumoLabel.Size = new System.Drawing.Size(121, 27);
+            this.consumoLabel.TabIndex = 1;
+            this.consumoLabel.Tag = "1";
+            this.consumoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // kmTextBox
+            // 
+            this.kmTextBox.Location = new System.Drawing.Point(69, 28);
+            this.kmTextBox.Name = "kmTextBox";
+            this.kmTextBox.Size = new System.Drawing.Size(125, 27);
+            this.kmTextBox.TabIndex = 2;
+            this.kmTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // litrosTextBox
+            // 
+            this.litrosTextBox.Location = new System.Drawing.Point(69, 102);
+            this.litrosTextBox.Name = "litrosTextBox";
+            this.litrosTextBox.Size = new System.Drawing.Size(125, 27);
+            this.litrosTextBox.TabIndex = 2;
+            this.litrosTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // CalcularConsumo
+            // 
+            this.AcceptButton = this.calcularButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 256);
+            this.Controls.Add(this.litrosTextBox);
+            this.Controls.Add(this.kmTextBox);
+            this.Controls.Add(this.consumoLabel);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.rendimentoLabel);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcularButton);
+            this.KeyPreview = true;
+            this.Name = "CalcularConsumo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcular Consumo";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CalcularConsumo_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcularButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label rendimentoLabel;
+        private System.Windows.Forms.Label Label5;
+        private System.Windows.Forms.Label consumoLabel;
+        private System.Windows.Forms.TextBox kmTextBox;
+        private System.Windows.Forms.TextBox litrosTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcular_Consumo_do_Carro
+{
+    public partial class CalcularConsumo : Form
+    {
+        public CalcularConsumo()
+        {
+            InitializeComponent();
+        }
+
+        //Método Validar Decimal
+        private decimal ValidarDecimal(TextBox txt)
+        {
+            try
+            {
+                return Convert.ToDecimal(txt.Text);
+            }
+            catch
+            {
+                txt.Focus();
+                txt.SelectAll();
+                throw new Exception("Preencha Corretamente o Campo" + txt.Name.ToUpper().Replace("TEXTBOX", ""));
+            }
+        }
+
+        //Método Limpar Tela
+        private void LimparTela()
+        {
+            foreach (Control ctl in this.Controls)
+            {
+                if (ctl is TextBox)
+                {
+                    ctl.Text = string.Empty;
+                }
+                else if (ctl is Label && Convert.ToInt32(ctl.Tag).Equals(1))
+                {
+                    ctl.Text = string.Empty;
+                }
+            }
+        }
+
+        private void calcularButton_Click(object sender, EventArgs e)
+        {
+            rendimentoLabel.ResetText();
+             consumoLabel.ResetText();
+             try
+             {
+                 decimal km = ValidarDecimal(kmTextBox);
+                 decimal litros = ValidarDecimal(litrosTextBox);
+                 decimal rendimento = km / litros;
+                 decimal consumo = litros / km;
+                 rendimentoLabel.Text = rendimento.ToString("N1");
+                 consumoLabel.Text = consumo.ToString("N3");
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show(ex.Message, "Alerta de Erro",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+             }
+        }
+
+        private void CalcularConsumo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                //Propriedade KeyPreview deve ser true
+                if (e.KeyChar == 27)
+                {
+                    LimparTela();
+                    kmTextBox.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de Erro",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
